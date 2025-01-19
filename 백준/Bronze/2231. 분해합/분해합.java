1d44b6f493b8cf2a316d861a775b869b0781bded
TejavaV3 <108499461+TejavaV3@users.noreply.github.com>
2025-01-20 00:21:00 +0900

    [Bronze II] Title: 분해합, Time: 168 ms, Memory: 17684 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -3,18 +3,28 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
+		
+		// 자릿수의 길이를 알기 위해 문자열로 입력받음
+		String str_n = sc.nextLine();	
+		
+		// 해당 문자열의 길이 변수
+		int n_len = str_n.length();
+		
+		// 문자열을 정수로 변환
+		int n = Integer.parseInt(str_n);
 		int result = 0;
 		
-		for(int i=0; i<n; i++) {
-			int m = i;	
-			int sum = 0;
+		// i는 가능한 최솟값인 n-9*n의 각 자릿수부터 시작
+		for(int i=(n-(n_len*9)); i<n; i++) {
+			int num = i;
+			int sum = 0; // 각 자릿수 더하기
 			
-			while(m!=0) {	
-				sum+= m % 10;	
-				m /= 10;	
+			while(num != 0) {
+				sum += num % 10; // 각 자릿수 더하기
+				num /= 10;				
 			}
 			
+			// i값과 각 자릿수 누적합이 같을 경우 (생성자를 찾았을 경우)
 			if(sum + i == n) {
 				result = i;
 				break;
