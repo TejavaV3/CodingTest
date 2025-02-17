57d44f2e440038b43a490e327cfb046a49373419
TejavaV3 <108499461+TejavaV3@users.noreply.github.com>
2025-02-17 20:03:11 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 164 ms, Memory: 17576 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		switch(a) {
+		case "SONGDO" : System.out.println("HIGHSCHOOL"); break;
+		case "CODE" : System.out.println("MASTER"); break;
+		case "2023" : System.out.println("0611"); break;
+		case "ALGORITHM" : System.out.println("CONTEST"); break;
+		
+		}
+	}
+}
