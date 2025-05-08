55e6ca02457f5b5ec2a5bad9439d2a2350c84227
TejavaV3 <108499461+TejavaV3@users.noreply.github.com>
2025-05-08 15:40:27 +0900

    [D3] Title: 조별과제, Time: 131 ms, Memory: 30,900 KB -BaekjoonHub

---
+++ SWEA/D3/13218. 조별과제/조별과제.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int t=1 ; t<=T; t++) {
+			
+			int n = sc.nextInt();
+			int result = n/3;
+		
+			System.out.println("#" + t + " " + result);
+		}
+	}
+}
