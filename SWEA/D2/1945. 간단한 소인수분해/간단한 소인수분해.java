61f1c0af6ef9a7b75815481243a139a9c4fe0ffa
jawa <108499461+TejavaV3@users.noreply.github.com>
2025-05-19 00:25:58 +0900

    [D2] Title: 간단한 소인수분해, Time: 98 ms, Memory: 26,624 KB -BaekjoonHub

--- SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.java
+++ SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.io.FileInputStream;
 
 class Solution {
 	public static void main(String args[]) throws Exception {
@@ -9,35 +8,19 @@ class Solution {
 
 		for (int test_case = 1; test_case <= T; test_case++) {
 			int n = sc.nextInt();
-			int a = 0;
-			int b = 0;
-			int c = 0;
-			int d = 0;
-			int e = 0;
-
-			while (n % 2 == 0) {
-				n = n / 2;
-				a++;
+			int[] pf = { 2, 3, 5, 7, 11 };
+			int[] count = new int[5];
+			for (int i = 0; i < count.length; i++) {
+				while (n % pf[i] == 0) {
+					count[i]++;
+					n = n / pf[i];
+				}
 			}
-			while (n % 3 == 0) {
-				n = n / 3;
-				b++;
+			System.out.print("#" + test_case);
+			for (int c : count) {
+				System.out.print(" " + c);
 			}
-			while (n % 5 == 0) {
-				n = n / 5;
-				c++;
-			}
-			while (n % 7 == 0) {
-				n = n / 7;
-				d++;
-			}
-			while (n % 11 == 0) {
-				n = n / 11;
-				e++;
-			}
-
-
-			System.out.println("#" + test_case + " " + a + " " + b + " " + c + " " + d + " " + e);
+			System.out.println();
 		}
 	}
 }
