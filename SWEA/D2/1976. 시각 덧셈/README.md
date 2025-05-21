94683c337c9f62967e86dde8bb8f2ebcdf59e320
jawa <108499461+TejavaV3@users.noreply.github.com>
2025-05-21 09:41:36 +0900

    [D2] Title: 시각 덧셈, Time: 93 ms, Memory: 26,240 KB -BaekjoonHub

---
+++ SWEA/D2/1976. 시각 덧셈/README.md
@@ -0,0 +1,15 @@
+# [D2] 시각 덧셈 - 1976 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PttaaAZIDFAUq) 
+
+### 성능 요약
+
+메모리: 26,240 KB, 시간: 93 ms, 코드길이: 522 Bytes
+
+### 제출 일자
+
+2025-05-21 09:41
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D2/1976. 시각 덧셈/시각 덧셈.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+class Solution {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T = sc.nextInt();
+
+		for (int test_case = 1; test_case <= T; test_case++) {
+			int h1 = sc.nextInt();
+			int m1 = sc.nextInt();
+			int h2 = sc.nextInt();
+			int m2 = sc.nextInt();
+			m1 += m2;
+			h1 += h2;
+
+			if (m1 >= 60) {
+				m1 = m1 - 60;
+				h1++;
+			}
+			if (h1 > 12) {
+				h1 -= 12;
+			}
+
+			System.out.println("#" + test_case + " " + h1 + " " + m1);
+		}
+	}
+}
