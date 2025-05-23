aa70f53292403165fd722ba0820c3ae2fcfc7b28
jawa <108499461+TejavaV3@users.noreply.github.com>
2025-05-23 17:34:23 +0900

    [D3] Title: [Professional] 쥬스 나누기, Time: 137 ms, Memory: 30,208 KB -BaekjoonHub

---
+++ SWEA/D3/5601. ［Professional］ 쥬스 나누기/［Professional］ 쥬스 나누기.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+class Solution {
+
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+
+		for (int tc = 1; tc <= T; tc++) {
+			int n = sc.nextInt();
+			System.out.print("#" + tc + " ");
+			for(int i=0; i<n; i++) {
+				System.out.print("1/"+ n);
+				if(i!=n) {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
